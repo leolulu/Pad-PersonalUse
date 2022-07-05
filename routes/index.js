7446229a7c9d22a76a5507c9e351139d4d591c66
surface@pro.3 <348699103@qq.com>
2022-07-05 12:18:06 +0800

    make sid fixed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var sid = shortid.generate()
+  var sid = 'default'
   res.render('index', { title: 'Welcome to Pad', buttonLbl: 'Get started', id: sid })
 })
 
