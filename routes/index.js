8ea8b0b791e5c9eb2f6b8343a69b605d181bfb94
surface@pro.3 <348699103@qq.com>
2022-11-23 23:22:25 +0800

    不重定向了，又改回来了

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,9 @@ var shortid = require('shortid')
 var router = express.Router()
 
 /* GET home page. */
-// router.get('/', function (req, res, next) {
-//   var sid = 'default'
-//   res.render('index', { title: 'Welcome to Pad', buttonLbl: 'Get started', id: sid })
-// })
-
 router.get('/', function (req, res, next) {
-  res.render('pad', {
-    title: 'Pad',
-    temp: '这是palceholder...开始写吧~'
-  })
+  var sid = 'default'
+  res.render('index', { title: 'Welcome to Pad', buttonLbl: 'Get started', id: sid })
 })
 
 module.exports = router
